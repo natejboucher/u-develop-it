6f04a89f8203283217c11b9a98f5967359edbacb
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-02-15 23:44:44 -0500

    IT WORKED

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const mysql = require('mysql2');
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,11 +7,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'zerotwo',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
