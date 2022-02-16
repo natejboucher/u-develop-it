35d0a73fb5146c5eaa4fd58f5531dc4b1aa49cbf
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-02-15 19:46:26 -0500

    added createconnection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,13 +7,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'localhost',
+      // Your MySQL password
+      password: 'zerotwo',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+  
 
-
-//Default response for any other request (not found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
